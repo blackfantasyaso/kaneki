c249dcde8af7d7b484aa79df01cabc8dea0e5443
aso1301023 <aso1301023@st.asojuku.ac.jp>
2015-06-16 10:29:50 +0900

    Revert "a"
    This reverts commit b6b2e2212f74ef7c0093d638f3fc30fd11777d2d.

--- Kaneki2/src/com/kadai/kaneki2/MainActivity.java
+++ Kaneki2/src/com/kadai/kaneki2/MainActivity.java
@@ -1,11 +1,11 @@
 package com.kadai.kaneki2;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class TreeNekko extends Activity {
+public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +25,10 @@ public class TreeNekko extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
 		return super.onOptionsItemSelected(item);
 	}
 }
